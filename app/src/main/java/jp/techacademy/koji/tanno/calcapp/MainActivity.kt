dbe6dac3886f58923fe4cc74e9ae0bfee26628da
koji.tanno <k.mouse0820@gmail.com>
2023-01-19 23:38:07 +0900

    ひとまず完成（Snackbarがキーボードで隠れるのだけ直したい）

--- app/src/main/java/jp/techacademy/koji/tanno/calcapp/MainActivity.kt
+++ app/src/main/java/jp/techacademy/koji/tanno/calcapp/MainActivity.kt
@@ -5,10 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.content.Intent
 import jp.techacademy.koji.tanno.calcapp.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -17,39 +16,67 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.calcButtonPlus.setOnClickListener {
-            if (binding.editText1.text.toString().isBlank() || binding.editText2.text.toString().isBlank()) {
-                Snackbar.make(binding.root, "数字を入力してください",Snackbar.LENGTH_INDEFINITE)
-                    .setAction("CANCEL"){}.show()
-            } else {
-                val intent = Intent(this, ResultActivity::class.java)
-                intent.putExtra("VALUE1", binding.editText1.text.toString().toDouble())
-                intent.putExtra("VALUE2", binding.editText2.text.toString().toDouble())
-                intent.putExtra("ENZAN", "+")
-                startActivity(intent)
+        binding.calcButtonPlus.setOnClickListener(this)
+        binding.calcButtonMinus.setOnClickListener(this)
+        binding.calcButtonMultiply.setOnClickListener(this)
+        binding.calcButtonDivide.setOnClickListener(this)
+
+    }
+
+    override fun onClick(v: View) {
+        if (binding.editText1.text.toString().isBlank() || binding.editText2.text.toString().isBlank()) {
+            Snackbar.make(binding.root, "数値を入力してください", Snackbar.LENGTH_INDEFINITE)
+                .setAction("CANCEL") {}.show()
+        } else {
+            var enzan: String = ""
+            when (v.id) {
+                R.id.calcButtonPlus -> enzan = "+"
+                R.id.calcButtonMinus -> enzan = "-"
+                R.id.calcButtonMultiply -> enzan = "*"
+                R.id.calcButtonDivide -> enzan = "/"
             }
-        }
-        binding.calcButtonMinus.setOnClickListener{
-            val intent = Intent(this,ResultActivity::class.java)
-            intent.putExtra("VALUE1",binding.editText1.text.toString().toDouble())
-            intent.putExtra("VALUE2",binding.editText2.text.toString().toDouble())
-            intent.putExtra("ENZAN","-")
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("VALUE1", binding.editText1.text.toString().toDouble())
+            intent.putExtra("VALUE2", binding.editText2.text.toString().toDouble())
+            intent.putExtra("ENZAN", enzan)
             startActivity(intent)
         }
-        binding.calcButtonMultiply.setOnClickListener{
-            val intent = Intent(this,ResultActivity::class.java)
-            intent.putExtra("VALUE1",binding.editText1.text.toString().toDouble())
-            intent.putExtra("VALUE2",binding.editText2.text.toString().toDouble())
-            intent.putExtra("ENZAN","*")
-            startActivity(intent)
-        }
-        binding.calcButtonDivide.setOnClickListener{
-            val intent = Intent(this,ResultActivity::class.java)
-            intent.putExtra("VALUE1",binding.editText1.text.toString().toDouble())
-            intent.putExtra("VALUE2",binding.editText2.text.toString().toDouble())
-            intent.putExtra("ENZAN","/")
+
+    }
+}
+
+        /*
+    binding.calcButtonPlus.setOnClickListener {
+        if (binding.editText1.text.toString().isBlank() || binding.editText2.text.toString().isBlank()) {
+            Snackbar.make(binding.root, "数字を入力してください",Snackbar.LENGTH_INDEFINITE)
+                .setAction("CANCEL"){}.show()
+        } else {
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("VALUE1", binding.editText1.text.toString().toDouble())
+            intent.putExtra("VALUE2", binding.editText2.text.toString().toDouble())
+            intent.putExtra("ENZAN", "+")
             startActivity(intent)
         }
     }
-
-}
+    binding.calcButtonMinus.setOnClickListener{
+        val intent = Intent(this,ResultActivity::class.java)
+        intent.putExtra("VALUE1",binding.editText1.text.toString().toDouble())
+        intent.putExtra("VALUE2",binding.editText2.text.toString().toDouble())
+        intent.putExtra("ENZAN","-")
+        startActivity(intent)
+    }
+    binding.calcButtonMultiply.setOnClickListener{
+        val intent = Intent(this,ResultActivity::class.java)
+        intent.putExtra("VALUE1",binding.editText1.text.toString().toDouble())
+        intent.putExtra("VALUE2",binding.editText2.text.toString().toDouble())
+        intent.putExtra("ENZAN","*")
+        startActivity(intent)
+    }
+    binding.calcButtonDivide.setOnClickListener{
+        val intent = Intent(this,ResultActivity::class.java)
+        intent.putExtra("VALUE1",binding.editText1.text.toString().toDouble())
+        intent.putExtra("VALUE2",binding.editText2.text.toString().toDouble())
+        intent.putExtra("ENZAN","/")
+        startActivity(intent)
+    }
+*/
